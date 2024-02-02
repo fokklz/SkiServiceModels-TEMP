14223bf658077075e4d02f002f1335327a248f9e
Fokko Vos <43372026+fokklz@users.noreply.github.com>
2024-02-02 10:38:47 +0100

    added mappings for serialization, for now in Extensions
    may be moved to the core of the BSON package since it will be needed for the models to work

---
+++ SkiServiceModels.BSON.Extensions/MongoDBInitialization.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using SkiServiceModels.BSON.Interfaces;
+using SkiServiceModels.BSON.Models;
+
+namespace SkiServiceModels.BSON.Extensions
+{
+    public class MongoDBInitialization
+    {
+        private static bool _isInitialized = false;
+
+        public static void RegisterMappings()
+        {
+            if (_isInitialized) return;
+
+            RegisterMapping<IPriority, Priority>();
+            RegisterMapping<IState, State>();
+            RegisterMapping<IService, Service>();
+            RegisterMapping<IUser, User>();
+            RegisterMapping<IOrder, Order>();
+
+            _isInitialized = true;
+        }
+
+        private static void RegisterMapping<TInterface, TConcrete>(bool root = false) where TConcrete : class, TInterface
+        {
+            BsonClassMap.RegisterClassMap<TConcrete>(cm =>
+            {
+                cm.AutoMap();
+                if(root) cm.SetIsRootClass(true);
+            });
+
+            BsonSerializer.RegisterSerializer(typeof(TInterface), new ImpliedImplementationInterfaceSerializer<TInterface, TConcrete>());
+        }
+    }
+}
